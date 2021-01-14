a04dc275481d4f74b655555008a4f193365955d6
Nilashish Roy <nilashishroyjoy@gmail.com>
2021-01-14 10:10:00 +0600

    Added Context

---
+++ app/src/main/java/com/example/attendancesystem/context/ApplicationContext.java
@@ -0,0 +1,46 @@
+package com.example.attendancesystem.context;
+
+import java.util.ArrayList;
+
+import android.app.Application;
+
+import com.example.attendancesystem.bean.AttendanceBean;
+import com.example.attendancesystem.bean.AttendanceSessionBean;
+import com.example.attendancesystem.bean.FacultyBean;
+import com.example.attendancesystem.bean.StudentBean;
+
+
+public class ApplicationContext extends Application {
+	private FacultyBean facultyBean;
+	private AttendanceSessionBean attendanceSessionBean;
+	private ArrayList<StudentBean> studentBeanList;
+	private ArrayList<AttendanceBean> attendanceBeanList;
+	
+	public FacultyBean getFacultyBean() {
+		return facultyBean;
+	}
+	public void setFacultyBean(FacultyBean facultyBean) {
+		this.facultyBean = facultyBean;
+	}
+	public AttendanceSessionBean getAttendanceSessionBean() {
+		return attendanceSessionBean;
+	}
+	public void setAttendanceSessionBean(AttendanceSessionBean attendanceSessionBean) {
+		this.attendanceSessionBean = attendanceSessionBean;
+	}
+	public ArrayList<StudentBean> getStudentBeanList() {
+		return studentBeanList;
+	}
+	public void setStudentBeanList(ArrayList<StudentBean> studentBeanList) {
+		this.studentBeanList = studentBeanList;
+	}
+	public ArrayList<AttendanceBean> getAttendanceBeanList() {
+		return attendanceBeanList;
+	}
+	public void setAttendanceBeanList(ArrayList<AttendanceBean> attendanceBeanList) {
+		this.attendanceBeanList = attendanceBeanList;
+	}
+	
+	
+
+}
